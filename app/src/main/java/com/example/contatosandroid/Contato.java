7659ff557e5f59646b73d60e11234ba909224931
Filipe Messias Silva <filipe.silva@transireserv.com>
2026-09-07 15:35:13 -0300

    Listas1.0 - Exercitando classe RecyclerView.Adapter

---
+++ app/src/main/java/com/example/contatosandroid/Contato.java
@@ -0,0 +1,22 @@
+package com.example.contatosandroid;
+
+public class Contato {
+
+    private String nome;
+    private String telefone;
+
+    public String getNome(){
+        return nome;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getTelefone(){
+        return telefone;
+    }
+    public void setTelefone(String tel){
+        this.telefone = tel;
+    }
+
+}
---
+++ app/src/main/java/com/example/contatosandroid/ContatoAdapter.java
@@ -0,0 +1,61 @@
+package com.example.contatosandroid;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class ContatoAdapter extends RecyclerView.Adapter<ContatoAdapter.ContatoAdapterHolder> {
+
+    private List<Contato> listaContatos;
+
+    public ContatoAdapter(List<Contato> listaContato){
+        this.listaContatos = listaContato;
+    }
+
+    @NonNull
+    @Override
+    /*
+    * onCreateViewHolder é um método qe constroi fisicamente um novo item na lista.
+    * Isso é, pega o molde item_contato.xml, transforma ele numa View(item de exibição), e devolve
+    * essa view já embrulhada num ContatoAdapterHolder, pronta para ser exibida ( mas ainda sem dados preenchidos, iss é função do onBindViewHolder
+    *
+    * */
+    public ContatoAdapter.ContatoAdapterHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_contato, parent, false); // assinatura
+
+        return new ContatoAdapterHolder(itemView);
+    }
+
+    /*
+    * Pega o contato certo da lista, de acordo com a posição, e preenche os TextViws daquele item
+    * visual especifico com o nome e telefone dele
+    * */
+    @Override
+    public void onBindViewHolder(@NonNull ContatoAdapter.ContatoAdapterHolder holder, int position) {
+        Contato contatoAtual = listaContatos.get(position);
+        holder.textViewNome.setText(contatoAtual.getNome());
+        holder.textViewTelefone.setText(contatoAtual.getTelefone());
+    }
+
+    @Override
+    public int getItemCount() {
+        return listaContatos.size();
+    }
+
+    public static class ContatoAdapterHolder extends RecyclerView.ViewHolder{
+
+        TextView textViewNome;
+        TextView textViewTelefone;
+        public ContatoAdapterHolder(@NonNull View itemView){
+            super(itemView);
+            textViewNome = itemView.findViewById(R.id.textViewNome);
+            textViewTelefone = itemView.findViewById(R.id.textViewTelefone);
+        }
+    }
+}
